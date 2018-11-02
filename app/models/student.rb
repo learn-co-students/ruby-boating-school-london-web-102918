3c04bd97aa85fb0900facaa3fe11dabf0803676b
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-02 15:03:12 +0000

    Did stuff

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
 class Student
 
+  @@all = []
+
+  attr_accessor :first_name
+
+  def initialize(first_name)
+    @first_name = first_name
+    @all << self
+  end
+
+# --- INSTANCE METHODS ----
+
+  def add_boating_test(test_name, student, b_test_status, instructor)
+    Boatingtest.new(test_name)
+  end
+
+
+# ----- CLASS METHODS -----
+
+  def self.find_student(name_input)
+    puts
+  end
+
+  def self.all
+    @@all
+  end
+
 end
