164e766a4588a8a66f5209c5f116fe16b5c91523
github username <github email address>
2018-11-06 22:06:51 +0000

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,35 @@
+# require 'pry'
 class Instructor
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.pass_student(student, test_name)
+    update = BoatingTest.all.find { |info| info.student == student && info.test_name == test_name}
+    update.test_status = "passed"
+  end
+
+  def self.fail_student(student, test_name)
+    update = BoatingTest.all.find { |info| info.student == student && info.test_name == test_name}
+    update.test_status = "failed"
+  end
+
+  def self.student_grade_percentage(student)
+    test_data = BoatingTest.all.select {|data| data.student == student}
+    all_tests = test_data.select {|test| test.test_name}
+    tests_passed = test_data.select {|test| test.test_status == "passed"}
+    test_percentage = (tests_passed.length.to_f / all_tests.length.to_f) * 100
+  end
+
+# binding.pry
 end
